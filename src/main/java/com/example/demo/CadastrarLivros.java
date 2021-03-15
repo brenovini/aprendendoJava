254c501e9fb3e4241bfb8432e7d42728621993f5
breno couto <brenovinicius123455.mga@gmail.com>
2021-03-15 19:43:24 -0300

    criado retorno

--- src/main/java/com/example/demo/CadastrarLivros.java
+++ src/main/java/com/example/demo/CadastrarLivros.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CadastrarLivros {
-    public void cadastrar(String titulo,int paginas,String autor){
+    public List<Livro> cadastrar(String titulo, int paginas, String autor){
             Livro livro = new Livro();
             livro.setTitulo(titulo);
             livro.setAutor(autor);
             livro.setPaginas(paginas);
         List<Livro>livros= new ArrayList<>();
         livros.add(livro);
+        return livros;
     }
+
 }
