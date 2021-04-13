6572dd661d2fa4b653f3b513eba3e4399260d24b
Alex Travagin Chatalov <alextc@ciandt.com>
2021-04-13 19:39:13 -0300

    criado o request

--- src/main/java/com/example/demo/Request.java
+++ src/main/java/com/example/demo/Request.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -47,7 +48,7 @@ public class Request {
     }
 
     @GetMapping("/livros")
-    public List<Usuario> getLivros(){
-        return UsuarioSingleton.getInstance().getUsuarios();
+    public List<Livro> getLivros(){
+        return new ArrayList<>();
     }
 }
